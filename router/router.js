7401e5005e9c6d76dc4c5a8e0f6cc77f9698d532
Torobekov Aliaskar <torobekov_a@IUCA.LOC>
2026-04-08 14:46:25 +0600

    docs: add middleware logic and route protection

---
+++ router/router.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import PostControllers from "../controllers/PostControllers.js";
+import AuthController from "../controllers/AuthController.js";
+import authMiddleware from "../middleware/authMiddleware.js";
+
+const router = new Router();
+
+router.post("/goods", authMiddleware, PostControllers.create);
+router.get("/goods", PostControllers.getAll);
+router.get("/goods/:id", PostControllers.getOne);
+router.put("/goods/:id", authMiddleware, PostControllers.update);
+router.delete("/goods/:id", authMiddleware, PostControllers.delete);
+router.post("/register", AuthController.register);
+router.post("/login", AuthController.login);
+router.get("/getUser", authMiddleware, AuthController.getUser);
+router.get("/CreateRoles", AuthController.CreateRoles);
+
+export default router;
